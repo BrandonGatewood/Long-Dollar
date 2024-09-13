3254ec66c5d83ccfdeb12b1dc4ac631a223eedea
Brandon Gatewood <90942107+BrandonGatewood@users.noreply.github.com>
2024-09-13 03:48:39 -0700

    changed add button on toolbar centered button and changed size to 24px

--- frontend/src/components/modals/AddColleagueModal.jsx
+++ frontend/src/components/modals/AddColleagueModal.jsx
@@ -61,7 +61,7 @@ const AddColleagueModal = () => {
     return (
         <>
 
-            <button type="button" className="button" onClick={ () => setIsActive(true) }>
+            <button type="button" className="button buttonAdd" onClick={ () => setIsActive(true) }>
                 <AddColleagueIcon className='iconWhite'/>
             </button> 
 
